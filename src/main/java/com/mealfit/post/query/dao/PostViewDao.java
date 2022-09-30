91405aeb806585c89f1088955219df27aa65d512
9JaHyun <jahyunnine@gmail.com>
2022-09-30 17:51:44 +0900

    Query 삭제

--- src/main/java/com/mealfit/post/query/dao/PostViewDao.java
+++
@@ -1,8 +0,0 @@
-package com.mealfit.post.query.dao;
-
-import com.mealfit.post.query.dto.PostDetailView;
-import org.springframework.data.repository.Repository;
-
-public interface PostViewDao extends Repository<PostDetailView, Long> {
-
-}
